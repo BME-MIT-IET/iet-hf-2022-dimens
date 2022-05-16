e9ef951529d3ecebbc52d4e40c21d7cfc2527af4
batka-gabor <batka.gabor@freemail.hu>
2022-05-16 18:49:21 +0200

    Corrected code smells in Game claass

--- src/main/java/asteroidgame/Game.java
+++ src/main/java/asteroidgame/Game.java
@@ -7,7 +7,7 @@ public abstract class Game {
 	/** Number of living settlers */
 	private static int numberOfSettlers = 0;
 	/** All steppable objects of the game */
-	private static ArrayList<Steppable> steppables = new ArrayList<>();
+	private static ArrayList<Steppable> steppables = new ArrayList<Steppable>();
 	/** Indicates whether the game is over or not */
 	private static boolean endGame = false;
 
@@ -55,12 +55,12 @@ public abstract class Game {
 			for (Steppable each : steppables) {
 				if (steppedSettlers < numberOfSettlers) {
 					Settler settler = (Settler) each;
-					Thread t = Controller.ButtonClick(settler);
+					Thread t = Controller.buttonClick(settler);
 					try {
 						t.join();
 					}
 					catch(Exception e) {}
-					settler.step(Controller.COMMAND);
+					settler.step(Controller.command);
 					steppedSettlers++;
 				} else {
 					each.step("");
@@ -86,12 +86,12 @@ public abstract class Game {
 	 * calling Field.initField(). Starts the game.
 	 */
 	public static void initGame() {
-		ArrayList<Entity> entitiesList = new ArrayList<>();
+		ArrayList<Entity> entities_list = new ArrayList<Entity>();
 		for (int i = 0; i < 4; i++) {
 			Settler settler = new Settler(null);
-			entitiesList.add(settler);
+			entities_list.add(settler);
 		}
-		Field.initField(entitiesList);
+		Field.initField(entities_list);
 		game();
 	}
 }
